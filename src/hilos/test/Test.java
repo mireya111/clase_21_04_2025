463e23937610efaf880fd6b80f6998f766a44be3
mireya111 <mireya.garcia@epn.edu.ec>
2025-04-28 22:58:50 -0500

    Hilos 3

--- src/hilos/test/Test.java
+++ src/hilos/test/Test.java
@@ -1,7 +1,7 @@
 import hilos.clase.*;
 
 public static void main(String[] args) {
-    /*ProcesoA a = new ProcesoA(100);
+    ProcesoA a = new ProcesoA(100);
     ProcesoB b = new ProcesoB("Juan");
     ProcesoC c = new ProcesoC(10);
     //ProcesoD d = new ProcesoD();
@@ -14,15 +14,15 @@ public static void main(String[] args) {
     b.start();
     c.start();
     d.start();
-    e.start();*/
-    /*Persona p = new Persona();
+    e.start();
+    Persona p = new Persona();
     p.setNombre("Mireya");
     p.setFechaNacimiento("08 de septiembre del 2004");
     p.setDireccion("La Forestal");
 
     System.out.println("Nombre: "+p.getNombre());
     System.out.println("Fecha de nacimiento: "+p.getFechaNacimiento());
-    System.out.println("Direccion: "+p.getDireccion());*/
+    System.out.println("Direccion: "+p.getDireccion());
     Runnable k = new ProcesoF();
     Thread f = new Thread(k);
     f.start();
--- src/hilos/vista/Ventana.java
+++ src/hilos/vista/Ventana.java
@@ -65,7 +65,6 @@ public class Ventana extends JFrame {
                             "Marca: " + a.getMarca() + "\n" +
                             "Modelo: " + a.getModelo() + "\n" +
                             "Precio: " + a.getPrecio());
-
         });
 
     }
